c79d6e4181ef4e0df618109b1638e8928a2d3223
班定悦 <bandingyue@ztgame.com>
2020-08-05 18:33:33 +0800

    Ban Upload

---
+++ Assets/0_Need/Tools/ExtensionTools.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BanSupport
+{
+
+	public static partial class Tools
+	{
+		public static void Select(this GameObject[] gameObjectArray, GameObject target)
+		{
+			foreach (var aGo in gameObjectArray)
+			{
+				aGo?.SetActive(aGo == target);
+			}
+		}
+
+		public static void Select(this GameObject[] gameObjectArray, int index)
+		{
+			for (int i = 0; i < gameObjectArray.Length; i++)
+			{
+				gameObjectArray[i]?.SetActive(i == index);
+			}
+		}
+
+		public static Vector3 CastXZ(this Vector3 vector3)
+		{
+			vector3.y = 0;
+			return vector3;
+		}
+
+		public static Vector3 CastXY(this Vector3 vector3)
+		{
+			vector3.z = 0;
+			return vector3;
+		}
+
+		public static Vector3 CastYZ(this Vector3 vector3)
+		{
+			vector3.x = 0;
+			return vector3;
+		}
+
+		public static bool Contains<T>(this IEnumerable<T> set, T t)
+		{
+			foreach (var aT in set)
+			{
+				if (ReferenceEquals(aT, t))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public static bool Contains<T>(this IEnumerable<T> set, Func<T, bool> conditionFunc)
+		{
+			foreach (var aT in set)
+			{
+				if (conditionFunc(aT))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+	}
+
+}
+
+
